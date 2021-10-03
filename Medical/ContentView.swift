fb5f4274cfe53b6c5c142bb77b49f6080d374e6b
yzg1025 <yzg862689234@163.com>
2021-10-03 12:19:43 +0800

    Initial Commit

---
+++ Medical/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Medical
+//
+//  Created by 于争光 on 2021/10/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Medical/MedicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicalApp.swift
+//  Medical
+//
+//  Created by 于争光 on 2021/10/3.
+//
+
+import SwiftUI
+
+@main
+struct MedicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
